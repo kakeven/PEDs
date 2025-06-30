55ccadf159f9f64c1de7c9b67edb81322d33edb4
kakeven <kevsnsilba@gmail.com>
2025-06-30 08:26:44 -0300

    12312

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.peds_tentativa_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.peds_tentativa_2 to javafx.fxml;
+    exports com.example.peds_tentativa_2;
+}
