456de6cee0a131ff4e5c64a57e36646d7a84776e
kakeven <kevsnsilba@gmail.com>
2025-07-01 18:33:49 -0300

    novo comit 2

--- src/main/java/Controller/Interface_cadastroController.java
+++ src/main/java/Controller/Interface_cadastroController.java
@@ -1,4 +1,29 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Interface_cadastroController {
+
+    @FXML
+    public Pane fundoInvisivel;
+    public void initialize(URL location, ResourceBundle resources) {
+        fundoInvisivel.requestFocus();
+    }
+
+    @FXML
+    private TextField campoUsuario;
+
+    @FXML
+    private PasswordField campoSenha;
+
+    @FXML
+    private Button botaoLogin;
+
 }
