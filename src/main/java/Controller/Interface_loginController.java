5eb674f726ae2db6808e14f853576699a596ca9b
kakeven <kevsnsilba@gmail.com>
2025-07-01 17:48:04 -0300

    tela login feito 2.0

--- src/main/java/Controller/Interface_loginController.java
+++ src/main/java/Controller/Interface_loginController.java
@@ -1,4 +1,34 @@
 package Controller;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
-public class Interface_loginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Interface_loginController implements Initializable {
+
+    @FXML
+    private TextField campoUsuario;
+
+    @FXML
+    private PasswordField campoSenha;
+
+    @FXML
+    private Button botaoLogin;
+    @FXML
+    public AnchorPane rootPane;
+    public void initialize(URL location, ResourceBundle resources) {
+        // Solicita que o foco vá para o AnchorPane (ou outro container) para tirar dos campos
+        rootPane.requestFocus();
+    }
+
+
+    @FXML
+    private void aoClicarEntrar() {
+        // deixar vazio — quem implementa é o responsável pela lógica
+    }
 }
